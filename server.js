68030994823d10284d4e69434ddef1402d5b3452
yousseflaribi <yousseflaribi10@gmail.com>
2025-04-05 13:51:22 +0100

    user-init

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+require('./config/connect'); // Database connection
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Routes
+const userRoutes = require('./routes/userRoutes');
+
+app.use('/api/users', userRoutes);
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
