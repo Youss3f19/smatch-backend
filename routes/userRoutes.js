135b0326bdd2f031396b5f6f580054a4134d5617
yousseflaribi <yousseflaribi10@gmail.com>
2025-04-05 13:52:22 +0100

    user-init

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
-const { upload } = require("../middleware/multerMiddleware");
 
 // Routes pour la gestion des utilisateurs
-router.post('/signup',upload.single('file'),UserController.signup);
+router.post('/signup',UserController.signup);
 router.post('/login', UserController.login);
 router.get('/:id', UserController.getUserById);    
-router.put('/:id', upload.single('file'), UserController.updateUser);     
+router.put('/:id', UserController.updateUser);     
 
 module.exports = router;
