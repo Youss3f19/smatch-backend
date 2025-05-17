483e0878668080b6676c4b8b189a9ef3bc75ab95
yousseflaribi <yousseflaribi10@gmail.com>
2025-05-17 14:24:53 +0100

    youssef8.0

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const UserController = require('../controllers/UserController');
 const auth = require('../middleware/auth'); 
 const authorize = require('../middleware/authorize'); 
-const upload = require('../middleware/upload'); 
+const upload = require('../middleware/multerMiddleware'); 
 
 // Routes pour la gestion des utilisateurs
 router.post('/signup', UserController.signup);
 router.post('/login', UserController.login);
 router.get('/:id', auth, UserController.getUserById);
-router.put('/:id', auth, upload.single('profilePicture'), UserController.updateUser);
+router.put('/:id', auth , UserController.updateUser);
 router.get('/', auth, authorize(['admin']), UserController.getAllUsers);
 router.put('/:id/upgrade-to-player', auth, UserController.upgradeToPlayer);
 
