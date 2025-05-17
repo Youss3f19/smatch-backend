6ab0d9eebdde0cd8b3c6915276547f7328f903e9
yousseflaribi <yousseflaribi10@gmail.com>
2025-05-17 12:53:33 +0100

    fix-users-controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -85,3 +85,13 @@ exports.updateUser = async (req, res) => {
     res.status(500).json({ message: 'Error updating user', error });
   }
 };
+
+// Get all users
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: 'Error retrieving users', error });
+  }
+};
